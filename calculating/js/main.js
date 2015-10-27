57d0372d0f0b368245406e58f54e2067fa3eaf06
Zheka <zhuravel.ea@gmail.com>
2015-10-27 11:06:56 +0200

    Initial commit

---
+++ calculating/js/main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Monster on 29.05.2015.
+ */
+
+//function flexiselSlider(){
+//    var percent = 0, bar = $('.transition-timer-carousel-progress-bar'), crsl = $('#myCarousel');
+//    function progressBarCarousel() {
+//        bar.css({width:percent+'%'});
+//        percent = percent +0.5;
+//        if (percent>100) {
+//            percent=0;
+//            crsl.carousel('next');
+//        }
+//    }
+//    crsl.carousel({
+//        interval: false,
+//        pause: false
+//    }).on('slid.bs.carousel', function () {percent=0;});var barInterval = setInterval(progressBarCarousel, 30);
+//}
+//
+//
+//function clickSolutionRedArrow(){
+//    $('.solution_red_arrow').on('click', function () {
+//        $('.solution_red_arrow,.solution_all_block').slideUp();
+//
+//    });
+//}
+//
+//$(document).ready(function(){
+//    //main page(ninislider)
+//    flexiselSlider();
+//    //solution page (red arrow)
+//    clickSolutionRedArrow();
+//});
---
+++ somax/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Monster on 29.05.2015.
+ */
+
+
+
+$(document).ready(function(){
+    $('.slider_blocks_list').glide();
+
+});
